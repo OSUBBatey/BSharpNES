975961f9ec525a59c9926b4d025d532c7195e696
batey.18 <batey.18@osu.edu>
2018-12-15 15:04:55 -0500

    Started Defining Memory Mapping

--- BSharpNESEmu/Class1.cs
+++ BSharpNESEmu/Class1.cs
@@ -6,24 +6,27 @@ using System.Threading.Tasks;
 
 namespace BSharpEmu
 {
-    public class MemoryRep
+    class CPUMemoryMap : IMemoryMap
     {
-        protected byte[] MemorySpace;
-        public MemoryRep(int size)
-        {
-            MemorySpace = new byte[size];
-        }
+        //Memory Address Range (0x0000 to 0x07FF)
+        //TODO: MAY MOVE INTERNAL MEMORY OUT OF MAP AND USE MAP STRICTLY FOR ADDRESS TRANSLATION
+        protected MemoryRep InternalRAM = new MemoryRep(0x800);
+        /*
+         * Remember to setup memory mirroring through enums or something         
+         * Mirrors are exact same size as Internal RAM and Range from: 
+         * 0x0800 to 0x0FFF  
+         * 0x1000 to 0x17FF
+         * 0x1800 to 0x1FFF 
+         */
 
-        public byte ReadFromMemory()
-        {
-            //TODO: NEW METHOD NAME? Program read logic
-            return 0;
-        }
+         //TODO: PPU REGISTERS OR REFERENCE TO THEM 0x2000 to 0x2007
+         //ALSO MIRRORS every 8 bytes from 0x2008 to 0x3FFF
 
-        public byte WriteToMemory()
-        {
-            //TODO: NEW METHOD NAME? Program write logic
-            return 0;
-        }
+        //TODO: APU I/O REGISTERS 0x4000 to 0x4017
+
+        //TODO: 0x4018 to 0x401F  CPU TEST MODE .. SEE NESDEV WIKI
+
+        //TODO: CARTRIDGE SPACE 
+
     }
 }
