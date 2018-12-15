8a2b143ea8b121e3b0d6be96cbeeb26a91ef94bf
batey.18 <batey.18@osu.edu>
2018-12-15 14:31:15 -0500

    Created Preliminary Memory Representation( No Logic yet)

---
+++ BSharpNESEmu/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSharpEmu
+{
+    public class MemoryRep
+    {
+        protected byte[] MemorySpace;
+        public MemoryRep(int size)
+        {
+            MemorySpace = new byte[size];
+        }
+
+        public byte ReadFromMemory()
+        {
+            //TODO: NEW METHOD NAME? Program read logic
+            return 0;
+        }
+
+        public byte WriteToMemory()
+        {
+            //TODO: NEW METHOD NAME? Program write logic
+            return 0;
+        }
+    }
+}
